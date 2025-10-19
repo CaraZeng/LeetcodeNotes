3236ef77400bf8bab01dad56c90039d3533b26d9
cara <carazengca@gmail.com>
2025-10-18 17:08:04 -0700

    Happy

---
+++ hash/set/1207_Unique_Number_of_Occurrences.py
@@ -0,0 +1,41 @@
+"""
+1207. Unique Number of Occurrences
+Given an array of integers arr, return true if the number of 
+occurrences of each value in the array is unique or false otherwise.
+
+Example 1:
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No
+two values have the same number of occurrences.
+
+Example 2:
+Input: arr = [1,2]
+Output: false
+
+Example 3:
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+Constraints:
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000
+"""
+
+# Key Note:
+# 1. use the len(set(freq.values()) == len(set(freq.values())) to
+# check whether there are duplicate values in dict.
+# 2. use Counter to count the freq.
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        num_freq = defaultdict(int)
+        for num in arr:
+            num_freq[num] += 1
+        return len(set(num_freq.values())) == len(num_freq.values())
+
+from collections import Counter
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        num_freq = Counter(arr)
+        return len(set(num_freq.values())) == len(num_freq.values())
