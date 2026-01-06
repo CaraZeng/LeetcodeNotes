4bc9b022e0894c2f4977c58d5bfaffcb35bd8208
cara <carazengca@gmail.com>
2026-01-06 13:43:26 -0800

    Update

---
+++ linked_list/merge/23_Merge_k_Sorted_Lists.py
@@ -0,0 +1,75 @@
+"""
+23. Merge k Sorted Lists
+You are given an array of k linked-lists lists, each linked-list is
+sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return 
+it.
+
+Example 1:
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted linked list:
+1->1->2->3->4->4->5->6
+
+Example 2:
+Input: lists = []
+Output: []
+
+Example 3:
+Input: lists = [[]]
+Output: []
+ 
+Constraints:
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-104 <= lists[i][j] <= 104
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 104.
+"""
+
+# Key Notes:
+# 1. its okay that dummy = ListNode(None) but None cant have next
+# so in case there is no mistake, we use ListNode(0)
+# 2. dont forget the base case in mergeSort
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        dummy = ListNode(0)
+        curr = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        if l1:
+            curr.next = l1
+        if l2:
+            curr.next = l2
+        return dummy.next
+    def mergeSort(self, lists, start, end):
+        if start == end:
+            return lists[start]
+        mid = (start + end) // 2
+        l1 = self.mergeSort(lists, start, mid)
+        l2 = self.mergeSort(lists, mid + 1, end)
+        return self.mergeTwoLists(l1, l2)
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return
+        return self.mergeSort(lists, 0, len(lists) - 1)
